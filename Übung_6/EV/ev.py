8d800ad31ff54e154fe02929d11f659e3b7e2b29
jaehkim99 <129087792+jaehkim99@users.noreply.github.com>
2025-07-08 14:11:06 +0200

    6.2

--- Übung_6/EV/ev.py
+++ Übung_6/EV/ev.py
@@ -21,5 +21,13 @@ def power_method(A: np.ndarray[complex], v0: np.ndarray[complex], tol: float=1e-
     """
     v = v0.astype(complex) # Guarantee that the initial vector is complex
     lambdas = np.zeros(max_iter, dtype=complex)
+    
+    for k in range(max_iter):
+        w = A @ v
+        v = w / np.linalg.norm(w)
+        lambdas[k] = np.vdot(v, A @ v) / np.vdot(v, v)  # Rayleig-Quotienten
+
+        if k > 0 and abs(lambdas[k] - lambdas[k-1]) < tol:
+            return np.array(lambdas[:k+1], dtype=complex), v
 
     return np.array(lambdas, dtype=complex), v
